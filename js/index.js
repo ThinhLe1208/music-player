581527cdab53aefb3df21e05fae0557d5a8971c7
ThinhLe1208 <lebaothinh1208@gmail.com>
2023-02-28 18:15:59 +0700

    thay đổi layout

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-// DOM element
+// DOM element base
 const audio = $('#audio');
-const titlePlayer = $('#title-player');
 const progress = $('#progress');
+const listOfSongs = $('.header__list-songs');
 
+// DOM element Content Left
+const contentSongImg = $('.content__song-img');
+const contentSongName = $('.content__song-name');
+const contentArtistName = $('.content__artist-name');
+const contentCoverImg = $('.content__cover-img');
+
+// DOM element Controller Buttons
 const playBtn = $('#play');
 const preBtn = $('#pre');
 const nextBtn = $('#next');
 const repeatBtn = $('#repeat');
 const shuffleBtn = $('#shuffle');
-
-const listSongs = $('#list-songs');
 
 // State
 let currentSong = 0;
@@ -24,61 +29,55 @@ let isShuffle = false;
 // Data
 const songs = [
     {
-        name: "1",
+        name: "We Don't Talk Anymore",
         singer: "Charlie Puth",
         path: "./music/Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez).mp3",
         image: "./image/g2.jpg"
     },
     {
-        name: "2",
+        name: "Perfect",
         singer: "Ed Sheeran",
         path: "./music/Ed Sheeran - Perfect.mp3",
         image: "./image/g4.jpg"
     },
     {
-        name: "3",
+        name: "Night Changes",
         singer: "One Direction",
         path: "./music/One Direction - Night Changes.mp3",
         image: "./image/g6.jpg"
     },
     {
-        name: "4",
-        singer: "Ed Sheeran",
-        path: "./music/Ed Sheeran - Perfect.mp3",
-        image: "./image/g4.jpg"
-    },
-    {
-        name: "5",
+        name: "We Don't Talk Anymore",
         singer: "Charlie Puth",
         path: "./music/Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez).mp3",
         image: "./image/g2.jpg"
     },
     {
-        name: "6",
+        name: "Perfect",
+        singer: "Ed Sheeran",
+        path: "./music/Ed Sheeran - Perfect.mp3",
+        image: "./image/g4.jpg"
+    },
+    {
+        name: "Night Changes",
         singer: "One Direction",
         path: "./music/One Direction - Night Changes.mp3",
         image: "./image/g6.jpg"
     },
     {
-        name: "7",
-        singer: "One Direction",
-        path: "./music/One Direction - Night Changes.mp3",
-        image: "./image/g6.jpg"
+        name: "We Don't Talk Anymore",
+        singer: "Charlie Puth",
+        path: "./music/Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez).mp3",
+        image: "./image/g2.jpg"
     },
     {
-        name: "8",
-        singer: "One Direction",
-        path: "./music/One Direction - Night Changes.mp3",
-        image: "./image/g6.jpg"
+        name: "Perfect",
+        singer: "Ed Sheeran",
+        path: "./music/Ed Sheeran - Perfect.mp3",
+        image: "./image/g4.jpg"
     },
     {
-        name: "9",
-        singer: "One Direction",
-        path: "./music/One Direction - Night Changes.mp3",
-        image: "./image/g6.jpg"
-    },
-    {
-        name: "10",
+        name: "Night Changes",
         singer: "One Direction",
         path: "./music/One Direction - Night Changes.mp3",
         image: "./image/g6.jpg"
@@ -88,34 +87,49 @@ const songs = [
 
 // Render
 function renderTitlePlayer() {
-    const stringHtml = `
-    <h1 class="card-title">${songs[currentSong].name}</h1>
-    <h5 class="mb-3">${songs[currentSong].singer}</h5>
-    <img class="image-song border" id="image-song" src="${songs[currentSong].image}" alt="${currentSong}">
-    `;
-
-    titlePlayer.innerHTML = stringHtml;
+    contentSongImg.src = songs[currentSong].image;
+    contentSongName.innerHTML = songs[currentSong].name;
+    contentArtistName.innerHTML = songs[currentSong].singer;
+    contentCoverImg.src = songs[currentSong].image;
 }
 
 function renderListSong() {
     const stringHtml = songs.map(function (song, index) {
         return `
-        <div class="alert ${index == currentSong ? 'alert-danger' : 'alert-primary'} d-flex align-item-start py-1 px-3" onclick="handleChooseSong(${index})">
-            <div class="d-flex flex-column">
-                <h3 class="m-0">${song.name}</h3>
-                <h5 class="m-0">${song.singer}</h5>
+        <li class="header__song" onclick="handleChooseSong(${index})">
+            <img class="header__song-img" src="${song.image}" alt="song-img">
+            <div class="header__song-content">
+                <div class="header__song-heading ${(index == currentSong && "active")}">
+                    <h3>${song.name}</h3>
+                    <div class="header__listening">
+                        <i class="fa-solid fa-headphones"></i>
+                        <span class="header__listening-count">166</span>
+                    </div>
+                </div>
+                <p><span>Singer:</span>${song.singer}</p>
             </div>
-        </div>
+        </li>
         `;
     });
-    listSongs.innerHTML = stringHtml.join('');
+
+    listOfSongs.innerHTML = stringHtml.join('');
+}
+
+function renderButtons() {
+    isRepeat ?
+        repeatBtn.style.opacity = '1' :
+        repeatBtn.style.opacity = '0.3';
+
+    isShuffle ?
+        shuffleBtn.style.opacity = '1' :
+        shuffleBtn.style.opacity = '0.3';
 }
 
 // ============ App ======================
 
 renderTitlePlayer();
 renderListSong();
-
+renderButtons();
 
 // =============== Buttons ========================
 
@@ -124,12 +138,12 @@ playBtn.onclick = function () {
     if (isPlaying) {
         isPlaying = false;
         audio.pause();
-        playBtn.innerHTML = 'Play';
+        playBtn.innerHTML = '<i class="fa-regular fa-circle-play"></i>';
         clearInterval(interval);
     } else {
         isPlaying = true;
         audio.play();
-        playBtn.innerHTML = 'Pause';
+        playBtn.innerHTML = '<i class="fa-regular fa-circle-pause"></i>';
     }
 };
 
@@ -152,7 +166,6 @@ preBtn.onclick = function () {
     } else {
         checkPosition('pre') && currentSong--;
     }
-    checkPosition('pre') && currentSong--;
 
     renderListSong();
     loadCurrentSong();
@@ -161,17 +174,13 @@ preBtn.onclick = function () {
 // Click repeat btn 
 repeatBtn.onclick = function () {
     isRepeat = !isRepeat;
-    isRepeat ?
-        repeatBtn.className = 'btn btn-warning' :
-        repeatBtn.className = 'btn btn-outline-warning';
+    renderButtons();
 };
 
 // Click shuffle btn
 shuffleBtn.onclick = function () {
     isShuffle = !isShuffle;
-    isShuffle ?
-        shuffleBtn.className = 'btn btn-info' :
-        shuffleBtn.className = 'btn btn-outline-info';
+    renderButtons();
 };
 
 // =============== Progress ========================
@@ -259,4 +268,4 @@ audio.onended = function () {
 
 
 // sua loi interval khong xoa duoc
-// xoay image-song
+// progress hien thi loi chay ra giua khi chuyen song
