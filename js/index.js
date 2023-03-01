feff44f3a0d8b51a4d4657f4cabe7ea8c9af4398
ThinhLe1208 <lebaothinh1208@gmail.com>
2023-03-01 23:22:00 +0700

    chỉnh sửa cấu trúc javascript

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+// =====================  =======================
 
 // DOM element base
 const audio = $('#audio');
 const progress = $('#progress');
 const listOfSongs = $('.header__list-songs');
 
-// DOM element Content Left
-const contentSongImg = $('.content__song-img');
-const contentSongName = $('.content__song-name');
-const contentArtistName = $('.content__artist-name');
-const contentCoverImg = $('.content__cover-img');
+// DOM element Display Left
+const displaySongImg = $('.display__song-img');
+const displaySongName = $('.display__song-name');
+const displayArtistName = $('.display__artist-name');
+const displayCoverImg = $('.display__cover-img');
 
 // DOM element Controller Buttons
 const playBtn = $('#play');
@@ -19,87 +20,89 @@ const nextBtn = $('#next');
 const repeatBtn = $('#repeat');
 const shuffleBtn = $('#shuffle');
 
-// State
-let currentSong = 0;
-let interval;
-let isPlaying = false;
-let isRepeat = false;
-let isShuffle = false;
+const app = {
 
-// Data
-const songs = [
-    {
-        name: "We Don't Talk Anymore",
-        singer: "Charlie Puth",
-        path: "./music/Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez).mp3",
-        image: "./image/g2.jpg"
-    },
-    {
-        name: "Perfect",
-        singer: "Ed Sheeran",
-        path: "./music/Ed Sheeran - Perfect.mp3",
-        image: "./image/g4.jpg"
-    },
-    {
-        name: "Night Changes",
-        singer: "One Direction",
-        path: "./music/One Direction - Night Changes.mp3",
-        image: "./image/g6.jpg"
-    },
-    {
-        name: "We Don't Talk Anymore",
-        singer: "Charlie Puth",
-        path: "./music/Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez).mp3",
-        image: "./image/g2.jpg"
-    },
-    {
-        name: "Perfect",
-        singer: "Ed Sheeran",
-        path: "./music/Ed Sheeran - Perfect.mp3",
-        image: "./image/g4.jpg"
-    },
-    {
-        name: "Night Changes",
-        singer: "One Direction",
-        path: "./music/One Direction - Night Changes.mp3",
-        image: "./image/g6.jpg"
-    },
-    {
-        name: "We Don't Talk Anymore",
-        singer: "Charlie Puth",
-        path: "./music/Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez).mp3",
-        image: "./image/g2.jpg"
-    },
-    {
-        name: "Perfect",
-        singer: "Ed Sheeran",
-        path: "./music/Ed Sheeran - Perfect.mp3",
-        image: "./image/g4.jpg"
-    },
-    {
-        name: "Night Changes",
-        singer: "One Direction",
-        path: "./music/One Direction - Night Changes.mp3",
-        image: "./image/g6.jpg"
-    }
-];
+    // State
+    currentIndex: 0,
+    isPlaying: false,
+    isRepeat: false,
+    isShuffle: false,
+    interval: null,
 
+    // Data
+    songs: [
+        {
+            name: "We Don't Talk Anymore",
+            singer: "Charlie Puth",
+            path: "./music/Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez).mp3",
+            image: "./image/g2.jpg"
+        },
+        {
+            name: "Perfect",
+            singer: "Ed Sheeran",
+            path: "./music/Ed Sheeran - Perfect.mp3",
+            image: "./image/g4.jpg"
+        },
+        {
+            name: "Night Changes",
+            singer: "One Direction",
+            path: "./music/One Direction - Night Changes.mp3",
+            image: "./image/g6.jpg"
+        },
+        {
+            name: "We Don't Talk Anymore",
+            singer: "Charlie Puth",
+            path: "./music/Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez).mp3",
+            image: "./image/g2.jpg"
+        },
+        {
+            name: "Perfect",
+            singer: "Ed Sheeran",
+            path: "./music/Ed Sheeran - Perfect.mp3",
+            image: "./image/g4.jpg"
+        },
+        {
+            name: "Night Changes",
+            singer: "One Direction",
+            path: "./music/One Direction - Night Changes.mp3",
+            image: "./image/g6.jpg"
+        },
+        {
+            name: "We Don't Talk Anymore",
+            singer: "Charlie Puth",
+            path: "./music/Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez).mp3",
+            image: "./image/g2.jpg"
+        },
+        {
+            name: "Perfect",
+            singer: "Ed Sheeran",
+            path: "./music/Ed Sheeran - Perfect.mp3",
+            image: "./image/g4.jpg"
+        },
+        {
+            name: "Night Changes",
+            singer: "One Direction",
+            path: "./music/One Direction - Night Changes.mp3",
+            image: "./image/g6.jpg"
+        }
+    ],
 
-// Render
-function renderTitlePlayer() {
-    contentSongImg.src = songs[currentSong].image;
-    contentSongName.innerHTML = songs[currentSong].name;
-    contentArtistName.innerHTML = songs[currentSong].singer;
-    contentCoverImg.src = songs[currentSong].image;
-}
+    // Render
+    renderTitlePlayer: function () {
+        displaySongImg.src = this.currentSong.image;
+        displaySongName.innerHTML = this.currentSong.name;
+        displayArtistName.innerHTML = this.currentSong.singer;
+        displayCoverImg.src = this.currentSong.image;
+    },
 
-function renderListSong() {
-    const stringHtml = songs.map(function (song, index) {
-        return `
-        <li class="header__song" onclick="handleChooseSong(${index})">
+    renderListSong: function () {
+
+        const stringHtml = this.songs.map((song, index) => {
+            return `
+        <li class="header__song ${index == this.currentIndex ? "active" : ""}" data-index="${index}">
             <img class="header__song-img" src="${song.image}" alt="song-img">
             <div class="header__song-content">
-                <div class="header__song-heading ${(index == currentSong && "active")}">
+                <div class="header__song-heading">
                     <h3>${song.name}</h3>
                     <div class="header__listening">
                         <i class="fa-solid fa-headphones"></i>
@@ -110,162 +113,188 @@ function renderListSong() {
             </div>
         </li>
         `;
-    });
+        });
 
-    listOfSongs.innerHTML = stringHtml.join('');
-}
+        listOfSongs.innerHTML = stringHtml.join('');
+    },
 
-function renderButtons() {
-    isRepeat ?
-        repeatBtn.style.opacity = '1' :
-        repeatBtn.style.opacity = '0.3';
+    renderButtons: function () {
+        this.isRepeat ?
+            repeatBtn.style.opacity = '1' :
+            repeatBtn.style.opacity = '0.3';
 
-    isShuffle ?
-        shuffleBtn.style.opacity = '1' :
-        shuffleBtn.style.opacity = '0.3';
-}
+        this.isShuffle ?
+            shuffleBtn.style.opacity = '1' :
+            shuffleBtn.style.opacity = '0.3';
+    },
 
-// ============ App ======================
-
-renderTitlePlayer();
-renderListSong();
-renderButtons();
-
-// =============== Buttons ========================
-
-// Click play/pause btn
-playBtn.onclick = function () {
-    if (isPlaying) {
-        isPlaying = false;
-        audio.pause();
-        playBtn.innerHTML = '<i class="fa-regular fa-circle-play"></i>';
-        clearInterval(interval);
-    } else {
-        isPlaying = true;
-        audio.play();
-        playBtn.innerHTML = '<i class="fa-regular fa-circle-pause"></i>';
-    }
-};
-
-// Click next btn
-nextBtn.onclick = function () {
-    if (isShuffle) {
-        currentSong = Math.floor(Math.random() * songs.length);
-    } else {
-        checkPosition('next') && currentSong++;
-    }
-
-    renderListSong();
-    loadCurrentSong();
-};
-
-// Click pre btn
-preBtn.onclick = function () {
-    if (isShuffle) {
-        currentSong = Math.floor(Math.random() * songs.length);
-    } else {
-        checkPosition('pre') && currentSong--;
-    }
-
-    renderListSong();
-    loadCurrentSong();
-};
-
-// Click repeat btn 
-repeatBtn.onclick = function () {
-    isRepeat = !isRepeat;
-    renderButtons();
-};
-
-// Click shuffle btn
-shuffleBtn.onclick = function () {
-    isShuffle = !isShuffle;
-    renderButtons();
-};
-
-// =============== Progress ========================
-
-// Change progress
-progress.onchange = function () {
-    audio.currentTime = progress.value * audio.duration / 100;
-};
-
-//  Progress changed when audio played
-audio.onplay = function () {
-    interval = setInterval(() => {
-        console.log('interval', interval);
-        progress.value = Math.floor(audio.currentTime / audio.duration * 100);
-    }, 1000);
-};
-
-// ===================== List of songs =========================
-
-function handleChooseSong(index) {
-    currentSong = index;
-    progress.value = 0;
-
-    renderTitlePlayer();
-    renderListSong();
-
-    audio.src = songs[currentSong].path;
-
-    if (isPlaying) {
-        audio.play();
-    }
-}
-
-// ============== Song ================
-
-// Load current song
-function loadCurrentSong() {
-    progress.value = 0;
-
-    renderTitlePlayer();
-    audio.src = songs[currentSong].path;
-
-    if (isPlaying) {
-        audio.play();
-    }
-}
-
-// Check song position
-function checkPosition(direction) {
-    switch (direction) {
-        case 'next':
-            if (currentSong == (songs.length - 1)) {
-                currentSong = 0;
-                return false;
+    defineProperties: function () {
+        Object.defineProperty(this, 'currentSong', {
+            get: function () {
+                return this.songs[this.currentIndex];
             }
-            return true;
-        case 'pre':
-            if (currentSong == 0) {
-                currentSong = songs.length - 1;
-                return false;
+        });
+    },
+
+    handleEvents: function () {
+        const _this = this;
+
+        // ===================== Buttons =======================
+
+        // Handle when click play/pause button
+        playBtn.onclick = function () {
+            if (_this.isPlaying) {
+                audio.pause();
+            } else {
+                audio.play();
             }
-            return true;
-        default:
-            console.error('checkPosition need argument');
-    }
-}
+        };
 
-// When song end
-audio.onended = function () {
-    if (isRepeat) {
-        loadCurrentSong();
-    }
+        // Handle when song is playing
+        audio.onplay = function () {
+            _this.isPlaying = true;
+            playBtn.innerHTML = '<i class="fa-regular fa-circle-pause"></i>';
+        };
 
-    if (isShuffle) {
-        currentSong = Math.floor(Math.random() * songs.length);
-    } else {
-        checkPosition('next') && currentSong++;
-    }
+        // Handle when song is paused
+        audio.onpause = function () {
+            _this.isPlaying = false;
+            playBtn.innerHTML = '<i class="fa-regular fa-circle-play"></i>';
+        };
 
-    renderListSong();
-    loadCurrentSong();
+        // Handle when click next button
+        nextBtn.onclick = function () {
+            if (_this.isShuffle) {
+                _this.randomSong();
+            } else {
+                _this.nextSong();
+            }
+
+            _this.loadCurrentSong();
+            _this.checkPlaying();
+        };
+
+        // Handle when click pre button
+        preBtn.onclick = function () {
+            if (_this.isShuffle) {
+                _this.randomSong();
+            } else {
+                _this.preSong();
+            }
+
+            _this.loadCurrentSong();
+            _this.checkPlaying();
+        };
+
+        // Handle when click repeat button 
+        repeatBtn.onclick = function () {
+            _this.isRepeat = !_this.isRepeat;
+            _this.renderButtons();
+        };
+
+        // Handle when click shuffle button
+        shuffleBtn.onclick = function () {
+            _this.isShuffle = !_this.isShuffle;
+            _this.renderButtons();
+        };
+
+        // ===================== Progress bar =======================
+
+        // Handle when change progress
+        progress.oninput = function () {
+            audio.currentTime = progress.value * audio.duration / 100;
+        };
+
+        // Handle progress is changed when audio is playing
+        audio.ontimeupdate = function () {
+            if (audio.duration) {
+                progress.value = audio.currentTime / audio.duration * 100;
+            }
+        };
+
+        // Handle when song end
+        audio.onended = function () {
+            if (_this.isRepeat) {
+                audio.play();
+            } else {
+                nextBtn.click();
+            }
+        };
+
+        // ===================== List of Songs =======================
+
+        listOfSongs.onclick = function (e) {
+            const songNode = e.target.closest('.header__song');
+
+            if (songNode) {
+                _this.currentIndex = songNode.getAttribute('data-index');
+
+                _this.loadCurrentSong();
+                _this.checkPlaying();
+            }
+        };
+    },
+
+    loadCurrentSong: function () {
+        this.renderTitlePlayer();
+        this.renderListSong();
+        audio.src = this.currentSong.path;
+    },
+
+    nextSong: function () {
+        if (this.currentIndex === (this.songs.length - 1)) {
+            this.currentIndex = 0;
+        } else {
+            this.currentIndex++;
+        }
+        this.loadCurrentSong();
+    },
+
+    preSong: function () {
+        if (this.currentIndex === 0) {
+            this.currentIndex = this.songs.length - 1;
+        } else {
+            this.currentIndex--;
+        }
+        this.loadCurrentSong();
+    },
+
+    randomSong: function () {
+        let randomIndex;
+        do {
+            randomIndex = Math.floor(Math.random() * this.songs.length);
+        } while (this.currentIndex === randomIndex);
+
+        this.currentIndex = randomIndex;
+        this.loadCurrentSong();
+    },
+
+    checkPlaying: function () {
+        if (this.isPlaying) {
+            audio.play();
+        }
+    },
+
+
+    start: function () {
+
+        // Define properties
+        this.defineProperties();
+
+        // Handle DOM events
+        this.handleEvents();
+
+        // Load song s information when first load app
+        this.loadCurrentSong();
+
+        // Render
+        this.renderTitlePlayer();
+        this.renderListSong();
+        this.renderButtons();
+    }
 };
 
-
-
+app.start();
 
 // sua loi interval khong xoa duoc
 // progress hien thi loi chay ra giua khi chuyen song
